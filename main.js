00fff8955213a64d617f08257cf580a1884498ca
pjy0122 <pzw0122@gmail.com>
2023-03-23 16:41:46 +0900

    quotes

--- main.js
+++ main.js
@@ -1,21 +1,78 @@
+const QUOTES = "quotes";
+
 function getTime() {
-    const time = document.querySelector(".time");
-    // 실무 js에서 많이 쓰는 querySelector
+  const time = document.querySelector(".time");
+  // 실무 js에서 많이 쓰는 querySelector
 
-    const newDate = new Date();
+  const newDate = new Date();
 
-    const hours = String(newDate.getHours()).padStart(2, "0");
-    const minutes= String(newDate.getMinutes()).padStart(2, "0");
-    const seconds = String(newDate.getSeconds()).padStart(2, "0");
+  const hours = String(newDate.getHours()).padStart(2, "0");
+  const minutes = String(newDate.getMinutes()).padStart(2, "0");
+  const seconds = String(newDate.getSeconds()).padStart(2, "0");
 
-    // if (seconds.toString().length === 1) {
-    //     seconds = "0" + seconds;
-    //   }
+  // if (seconds.toString().length === 1) {
+  //     seconds = "0" + seconds;
+  //   }
 
-    // time.innerText = hours + ":" + minutes + ":" + seconds;
-    time.innerText = `${hours}:${minutes}:${seconds}`;
+  // time.innerText = hours + ":" + minutes + ":" + seconds;
+  time.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getTime();
 
 setInterval(getTime, 1000);
+
+function getQuotes() {
+  const quotesMsg = document.querySelector(".quotesMsg");
+
+  let savedQuotes = localStorage.getItem(QUOTES);
+
+  if (!savedQuotes) {
+    localStorage.setItem(
+      QUOTES,
+      JSON.stringify([
+        "열심히 살지맙시다.",
+        "그래도 열심히 살아야지.",
+        "열심히 살면 뭐해~",
+        "열심히 살면 반드시 빛이 온다.",
+      ])
+    );
+
+    savedQuotes = localStorage.getItem(QUOTES);
+  }
+
+  let quotesArray = JSON.parse(savedQuotes);
+
+  quotesMsg.innerText =
+    quotesArray[Math.floor(Math.random() * quotesArray.length)];
+}
+
+getQuotes();
+
+function onClickAdd() {
+  const newQuotes = document.querySelector(".newQuotes");
+
+  newQuotes.style.display = "inline-block";
+}
+
+function onClickRegist() {
+  const quotesMsg = document.querySelector(".quotesMsg");
+  const newQuotes = document.querySelector(".newQuotes");
+  const newQuotesInput = document.querySelector(".newQuotesInput");
+
+  if (!newQuotesInput.value) {
+    return;
+    // if문에서 아무것도 안적고 return을 하면 다음것이 실행안되게함 (함수를 끝내는 용도이기도함)
+  }
+
+  let savedQuotes = localStorage.getItem(QUOTES);
+
+  let quotesArray = JSON.parse(savedQuotes);
+  quotesArray.push(newQuotesInput.value);
+
+  localStorage.setItem(QUOTES, JSON.stringify(quotesArray));
+
+  quotesMsg.innerHTML = `<span style="color:red;">${newQuotesInput.value}<span>`;
+  newQuotes.style.display = "none";
+  newQuotesInput.value = "";
+}
